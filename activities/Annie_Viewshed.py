9724695cb0569e192d5739f4c979e40e7b895d02
epauluk92 <98964783+epauluk92@users.noreply.github.com>
2022-03-14 17:38:33 -0400

    Add files via upload

---
+++ activities/Annie_Viewshed.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_viewshed(scanned_elev, env, **kwargs):
+    coordinates = [638830, 220150]
+    gs.run_command('r.viewshed', input=scanned_elev, output='viewshed', coordinates=coordinates, observer_elevation=2, flags='b', env=env)
+    gs.run_command('r.colors', map='viewshed', color='grey')
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    
+    run_viewshed(scanned_elev=elevation, env=env)
+
+
+if __name__ == "__main__":
+    main()
