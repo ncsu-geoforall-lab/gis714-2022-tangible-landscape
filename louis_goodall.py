7e3df3933cda2edef69b17ac6197beae7ff24ee9
lagoodal <97845125+lagoodal@users.noreply.github.com>
2022-04-06 09:23:13 -0400

    Add files via upload

---
+++ louis_goodall.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_contour(scanned_elev, env, **kwargs):
+    gs.run_command("r.contour", input=scanned_elev, output="contours", levels=[15,30,45])
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    run_contour(scanned_elev=elev_resampled, env=env., )
+
+if __name__ == '__main__':
+    main()
