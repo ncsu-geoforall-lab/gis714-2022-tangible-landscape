22fc4a7b7bbd422b30b17539c57ce66e1da6ddc0
Andrew Shannon <86684145+andrewcshannon@users.noreply.github.com>
2022-04-06 11:50:42 -0400

    Update after running Black

--- activities/Andrew_Aspect.py
+++ activities/Andrew_Aspect.py
@@ -6,15 +6,34 @@ import grass.script as gs
 
 
 def run_aspect_class(scanned_elev, env, **kwargs):
-    gs.run_command("r.slope.aspect", elevation=scanned_elev, aspect="aspect_val", 
-                   min_slope=8.0, flags="n", env=env)
+    gs.run_command(
+        "r.slope.aspect",
+        elevation=scanned_elev,
+        aspect="aspect_val",
+        min_slope=8.0,
+        flags="n",
+        env=env,
+    )
     # reclassify using rules passed as a string to standard input
-    # 0:22.5:1 means reclassify interval 0 to 22.5 percent of aspect to category 1 
-    rules = ['0:22.5:1', '22.5:67.5:1', '67.5:292.5:NULL', '292.5:337.5:1', '337.5:360:1', '*:-9999:NULL']
-    gs.write_command('r.recode', input='aspect_val', output='aspect_class',
-                          rules='-', stdin='\n'.join(rules), env=env)
+    # 0:22.5:1 means reclassify interval 0 to 22.5 percent of aspect to category 1
+    rules = [
+        "0:22.5:1",
+        "22.5:67.5:1",
+        "67.5:292.5:NULL",
+        "292.5:337.5:1",
+        "337.5:360:1",
+        "*:-9999:NULL",
+    ]
+    gs.write_command(
+        "r.recode",
+        input="aspect_val",
+        output="aspect_class",
+        rules="-",
+        stdin="\n".join(rules),
+        env=env,
+    )
     # set new color table: blue - white
-    gs.run_command('r.colors', map='aspect_class', color='wave', env=env)
+    gs.run_command("r.colors", map="aspect_class", color="wave", env=env)
 
 
 def main():
