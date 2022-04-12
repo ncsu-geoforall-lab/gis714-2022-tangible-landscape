eff21ba5dab2281080a5a2503229c3bf918d9f74
varunkt91 <36293441+varunkt91@users.noreply.github.com>
2022-04-12 17:05:42 -0400

    TL activity Varun

--- activities/varun.py
+++ activities/varun.py
@@ -5,8 +5,12 @@ import os
 import grass.script as gs
 
 
-def run_slope(scanned_elev, env, **kwargs):
-    gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
+def run_lake(scanned_elev, env, **kwargs):
+    coordinates = [638830, 220150]
+    res = gs.raster_what(map=scanned_elev, coord=[coordinates])
+    elev_value = float(res[0][scanned_elev]['value'])
+    gs.run_command('r.lake', elevation=scanned_elev, lake='output_lake',
+                   coordinates=coordinates, water_level=elev_value + 5, env=env)
 
 
 def main():
@@ -17,7 +21,7 @@ def main():
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
 
-    run_slope(scanned_elev=elev_resampled, env=env)
+    run_lake(scanned_elev=elev_resampled, env=env)
 
 
 if __name__ == "__main__":
