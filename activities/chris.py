97652765cae93f3eb798995fc53249bfc66da223
cdunsta <cdunsta@ncsu.edu>
2022-04-11 21:33:19 -0700

    fixes

--- activities/chris.py
+++ activities/chris.py
@@ -24,7 +24,7 @@ def main():
     gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
 
-    run_contours(scanned_elev=elev_resampled, env=env)
+    run_curvatures(scanned_elev=elev_resampled, env=env)
 
 
 if __name__ == "__main__":
